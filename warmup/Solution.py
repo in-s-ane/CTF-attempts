f8a6ac697b09da68b25bef9cf14ea38206da8496
Yicheng Wang <alex.wyc2098@protonmail.ch>
2014-12-09 19:20:50 -0500

    solution updated

---
+++ warmup/Solution.py
@@ -0,0 +1,4 @@
+# It seems like this is always one of the first problems on any ctf...
+# Simple hex --> ascii translation
+
+print "41444354465f57334c43304d335f37305f414443374632303134".decode("hex")
