36079da07eb061cd2f7995432ed5a5f184b4325f
James Wang <jameswang9909@hotmail.com>
2016-03-20 08:13:45 -0400

    Solve Colossal QRCode Adventure

--- Colossal-QRCode-Adventure_80/solution.sh
+++ Colossal-QRCode-Adventure_80/solution.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-set -e
-
-url="http://web.lasactf.com:14339/entrance.svg"
+if [[ $1 ]]; then
+    url=$1
+else
+    url="http://web.lasactf.com:14339/entrance.svg"
+fi
 while :; do
-    response=$(curl -s $url -m 5)
+    response=$(curl -s $url)
     if [[ ${#response} == 0 ]]; then
         echo "Unable to fetch anything..."
-        return
+        exit
     fi
     inkscape -z -e qr.png <(echo $response)
     contents=$(zbarimg qr.png)
     if ! echo $contents | grep "lasactf{" > /dev/null; then
-        url=$contents
-        echo $url
+        url=$(echo $contents | sed "s/QR-Code://g")
+        echo "Going to $url"
     else
         echo "Got the flag!"
         echo $contents
+        break
     fi
 done
+
+# your flag is: 'lasactf{a_maze_of_twisty_little_passages}'
